780a72f43fe0237fd667999c9f49debd65e6c574
nasteka <oleg.nasteka@gmail.com>
2018-11-04 01:29:08 +0200

    parent class ShopProduct and extends CDProduct & BookProduct

--- index.php
+++ index.php
@@ -12,18 +12,23 @@
 
 <?php
 
-class shopProduct{
-    public $title = "Стандартный товар";
-    public $producerMainName = "Фамилия автора";
-    public $producerFirstName = "Фамилия автора";
-    public $price = 0;
+class ShopProduct{
+    public $numPages;
+    public $playLength;
+    public $title;
+    public $producerMainName;
+    public $producerFirstName;
+    public $price;
 
-    function __construct($title, $firstName, $mainName, $price)
+    function __construct($title, $firstName, $mainName, $price,
+                        $numPages = 0, $playLength =0)
     {
         $this->title = $title;
         $this->producerFirstName = $firstName;
         $this->producerMainName = $mainName;
         $this->price = $price;
+        $this->numPages = $numPages;
+        $this->playLength = $playLength;
     }
 
     function getProducer()
@@ -32,10 +37,58 @@ class shopProduct{
             . "{$this->producerMainName}";
     }
 
+    function getSummaryLine()
+    {
+        $base = "$this->title ( {$this->producerMainName}, ";
+        $base .= "{$this->producerFirstName} )";
+        return $base;
+    }
 }
 
-$product1 = new shopProduct("Собачье сердце", "Михаил", "Булгаков", 5.99);
-print "Автор: {$product1->getProducer()}\n";
+class CDProduct extends ShopProduct{
+    function getPlayLength(){
+        return $this->playLength;
+    }
+    function getSummaryLine(){
+        $base = "{$this->title} ( {$this->producerMainName}, ";
+        $base .= "{$this->producerFirstName} )";
+        $base .= ": Время звучания - {$this->playLength}";
+        return $base;
+    }
+}
+
+class BookProduct extends ShopProduct{
+    function getNumberOfPages(){
+        return $this->numPages;
+    }
+}
+
+class shopProductWriter {
+    public function write( ShopProduct $shopProduct )
+    {
+        $str = "{$shopProduct->title}: "
+                .$shopProduct->getProducer()
+                ." ({$shopProduct->price})\n";
+        print $str;
+    }
+}
+
+
+
+$product1 = new ShopProduct("Собачье сердце",
+    "Михаил", "Булгаков", 5.99);
+//print "Автор: {$product1->getProducer()}\n";
+
+$writer = new ShopProductWriter();
+$writer->write( $product1 );
+
+echo "<br><br>";
+
+$product2 = new CDProduct("Пропавщий без вести",
+    "Группа", "ДДТ",
+    10.99, null, 60.33);
+
+print "Исполнитель: {$product2->getProducer()}\n";
 
 ?>
 
